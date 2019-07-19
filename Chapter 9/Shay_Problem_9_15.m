9547cfba6df555d413e9a4060f6819ab4e4513c6
ShaySackett <11085793+ShaySackett@users.noreply.github.com>
2019-07-18 23:23:31 -0700

    Add files via upload

---
+++ Chapter 9/Shay_Problem_9_15.m
@@ -0,0 +1,7 @@
+%Shay Sackett MATLAB ENGR 156
+%Problem 9.15 asks us to create an approximation of sin(x)
+
+
+
+n = 1:2:20;
+n.^(-1)
--- Chapter 9/Shay_Problem_9_6.m
+++ Chapter 9/Shay_Problem_9_6.m
@@ -1,21 +1,21 @@
 %Shay Sackett MATLAB ENGR 156
 %Problem 9.6 create and plot a fibonacci sequence defined y the user
 
-a = input('Enter the first number of the fibonacci sequence \n');
-b = input('Enter the second number of the fibonacci sequence \n');
-c = input('How many numbers would you like in the fibonacci sequence? \n');
+a = input('Enter the first number of the fibonacci sequence \n'); %Ask user for first input
+b = input('Enter the second number of the fibonacci sequence \n'); %Ask user for second input
+c = input('How many total numbers would you like in the fibonacci sequence? \n');  %Ask user for total # of elements in the sequence
 
-i = 0;
-seq = 0;
-d = 0;
+i = 0;  %initialize index #
+seq = [a, b];   %Store the first two values in the sequence
+d = 0;  %initialize d
 
-for i = 1:length(c)
-    d = a + b;
-    seq(i) = d;
-    a = b;
-    b = d;
+for i = 3:c       %for loop, for index starting at position 3, to c amount
+    d = a + b;   %find the next value in the fibonacci sequence 
+    seq(i) = d;  %add the next value of the sequence to the total sequence vector
+    a = b;       %set up b for the next sequence
+    b = d;       %set up d for the next sequence
 end
 
 
-seq
-%polarplot(i, seq)
+theta = 1:c;          %write theta to be a vector of indices with the same length as seq. 
+polarplot(theta, seq) %make a polar coordinate plot of theta vs seq.
