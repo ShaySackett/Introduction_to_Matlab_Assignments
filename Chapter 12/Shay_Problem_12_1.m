f8904d5c519cd034c5b1ace9a534ff7fcf6027d0
ShaySackett <11085793+ShaySackett@users.noreply.github.com>
2019-07-29 16:03:52 -0700

    Add files via upload

---
+++ Chapter 12/Shay_Problem_12_1.m
@@ -0,0 +1,11 @@
+%Shay Sackett ENGR 156 Matlab
+%Problem 12.1, symbolic variables
+
+syms a b c d x %create the symbolic variables
+
+% Part a - d: 
+
+se1 = x^3 - 3*x^2 +x  %create expression 1
+se2 = sin(x) + tan(x) %create expression 2
+se3 = (2*x^2 - 3*x - 2)/(x^2 - 5*x) %create expression 3
+se4 = (x^2 - 9)/(x + 3) %create expression 4
---
+++ Chapter 12/Shay_Problem_12_12.m
@@ -0,0 +1,9 @@
+%Shay Sackett ENGR 156 Matlab
+%Problem 12.12, kinetic energy
+
+syms ke m v;
+
+kinetic_energy = ke == .5*m*v^2
+
+
+
