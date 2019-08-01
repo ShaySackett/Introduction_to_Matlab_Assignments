7ad978007973f8938ad4cf2d8c1a929b3819aa03
ShaySackett <11085793+ShaySackett@users.noreply.github.com>
2019-08-01 15:37:08 -0700

    Add files via upload

--- Chapter 13/Shay_Problem_13_8.m
+++ Chapter 13/Shay_Problem_13_8.m
@@ -1,6 +1,29 @@
 %Shay Sackett ENGR 156 Matlab
 %Problem 13.8, polyfit
 
-n = 1; R = 8.314; V = 1:10; P; %Initialize constants
+n = 1; R = 8.314;                %initialize values
+P = [2494 1247 831 623 499 416]; %Pressure values
+V = 1:6;                         %Volume values
+X = 1./V;
 
-polyfit(1./V,P,1)
+line_eq_coefficents = polyfit(X,P,1)  %Linear curve fitting, find coefficents of line
+
+line_eq_coefficents(1)./(n*R)  %nrt = nrt --> 2493/8.314 = 300
+
+%% Part B: 
+%plot the equations 
+
+slope_equation = line_eq_coefficents(1).*X + line_eq_coefficents(2); %find equation for slope of the line
+plot(X, P, 'o', X,slope_equation)  %plot 1/v on x axis, pressure on y axis
+
+%% Part C: 
+%Repeat above part using linear regression through 0
+
+coefficents = P/X  %Send equation for curve-fit equation/line through 0. P/X = X\'P' according to Matlab documentation, P/X worked better than X\'P'
+eq_2 = coefficents.*X; %Get all the 'y' values for the plot
+
+clf %clear figure
+plot(X,P,'o', X, eq_2)  %The graph through 0 and the graph not through 0 are virtually indistinguishable, although if you uncomment the below plot and zoom in far enough one can see they are very slightly different
+
+
+%plot(X,P,'o', X, eq_2, X, slope_equation)
---
+++ Chapter 13/Shay_Problem_13_9.m
@@ -0,0 +1,43 @@
+%Shay Sackett ENGR 156 Matlab
+%Problem 13.9, resistance is futile
+
+%Part A: 
+clear, clc
+R = [10 15 25 40 65 100]; %resistance values
+I = [11.11 8.04 6.03 2.77 1.97 1.51]; %Current values
+
+plot(R,I,'o') %plot resistance vs current
+title('Resistance vs. Current') %add title
+xlabel('Resistance') %add x axis label
+ylabel('Current') %add y axis label
+grid on %turn grid on
+
+%% Part B: 
+
+%another plot
+
+C = 1./R; %conductance
+
+plot(C,I,'o') % plot 1/R vs I 
+grid on %turn grid on
+title('1/R vs I') %add title
+xlabel('1/R') %add x axis label
+ylabel('I') %add y axis label
+
+%% Part C:
+
+%use polyfit to calculate coefficents of line of regression
+
+coefficients = polyfit(C,I,1);  %apply polyfit to find coefficients, slope of the line is the applied voltage
+
+v = coefficients(1); %find voltage 
+
+%% Part D: 
+
+%Use polyval to find current values base on resistors used
+
+current = polyval(v, R)  % calculate current for each resistor value at the applied voltage.
+
+
+
+
